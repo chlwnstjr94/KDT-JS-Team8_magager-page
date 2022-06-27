72d134a91f05f6bf38cfa8a4e5b8548ad9479152
chlwnstjr94 <chlwnstjr940404@gmail.com>
2022-06-27 17:28:51 +0900

    feat: add the footer

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,17 @@
 <template>
   <TheHeader />
   <RouterView />
+  <TheFooter />
 </template>
 
 <script>
 import TheHeader from '~/components/TheHeader'
+import TheFooter from '~/components/TheFooter.vue'
 
 export default {
   components: {
-    TheHeader
+    TheHeader,
+    TheFooter
   }
 }
 </script>
