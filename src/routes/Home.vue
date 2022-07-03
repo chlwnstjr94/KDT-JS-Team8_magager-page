2a3d493b105d713c14ca4afddd5dea8ce4e4a8db
chlwnstjr94 <chlwnstjr940404@gmail.com>
2022-07-03 22:48:54 +0900

    feat: Modify routes index.js

--- src/routes/Home.vue
+++ src/routes/Home.vue
@@ -1,9 +1,25 @@
 <template>
-  <h1>Home page..</h1>
+  <main>
+    <h1>Home page..</h1>
+  </main>
 </template>
 
 <script>
+import { mapStores } from 'pinia'
+import { useUserStore } from '~/store/user'
 export default {
-
+  created() {
+    //this.userStore.authUser()
+  },
+  computed: {
+    ...mapStores(useUserStore),
+  },
+  watch: {},
 }
 </script>
+
+<style lang="scss" scoped>
+main {
+  height: 100vh;
+}
+</style>
