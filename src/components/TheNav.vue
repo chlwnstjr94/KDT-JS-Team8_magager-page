d089ba0f57d96c61312b3c82ba7f24792e310002
chlwnstjr94 <chlwnstjr940404@gmail.com>
2022-07-03 22:47:22 +0900

    feat: Add TheNav

---
+++ src/components/TheNav.vue
@@ -0,0 +1,54 @@
+<template>
+    <nav>
+      <div class="leftnav__inner">
+        <RouterLink v-if="!userStore.user" to="/login" class="nav-profile">
+          <div class="profile-big">
+            <img src="profile_default.png" alt="profile-default" />
+          </div>
+          <div class="profile-name">로그인</div>
+        </RouterLink>
+        <RouterLink v-else to="/mypage" class="nav-profile">
+          <div class="profile-big">
+            <img src="profile_default.png" alt="profile-default" />
+          </div>
+          <div class="profile-name">{{ this.userStore.user.displayName }}</div>
+        </RouterLink>
+        <ul class="list-nav">
+          <li>
+            <RouterLink to="/allproduct">모든 제품 조회</RouterLink>
+          </li>
+          <li>
+            <RouterLink to="/addproduct">제품 추가</RouterLink>
+          </li>
+        </ul>
+        <ul class="list-aside">
+          <li v-if="userStore.user" @click="userStore.logoutUser">
+            <RouterLink to="/">로그아웃</RouterLink>
+          </li>
+          <li v-if="userStore.user">
+            <RouterLink to="/user">정보 수정</RouterLink>
+          </li>
+        </ul>
+      </div>
+    </nav>
+</template>
+
+<script>
+import { mapStores } from 'pinia'
+import { useUserStore } from '~/store/user'
+export default {
+  created() {
+    //this.userStore.authUser()
+  },
+  computed: {
+    ...mapStores(useUserStore),
+  },
+  watch: {},
+}
+</script>
+
+<style lang="scss" scoped>
+nav {
+  width: 230px;
+}
+</style>
