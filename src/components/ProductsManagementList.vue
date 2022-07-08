b4e16b4bba8f05d3e7c430fe6138dffca12e46d0
chlwnstjr94 <chlwnstjr940404@gmail.com>
2022-07-08 16:56:31 +0900

    feat: Add ProducetsManagementList

---
+++ src/components/ProductsManagementList.vue
@@ -0,0 +1,34 @@
+<template>
+  <RouterLink to="/editproduct">
+    <ion-icon name="create-outline"></ion-icon>
+  </RouterLink>
+  <img 
+    v-if="product.thumbnail"
+    :src="product.thumbnail" 
+    :alt="product.title">
+  <h4>{{ product.title}}</h4>
+</template>
+
+<script>
+import { mapStores } from 'pinia'
+import { useIndexStore } from '~/store'
+
+export default {
+  props: {
+    product: {
+      type: Object,
+      default: () => ({})
+    }
+  },
+  computed: {
+    ...mapStores(useIndexStore)
+  }
+}
+</script>
+
+<style scoped lang="scss">
+img {
+    width: 100px;
+    height: 100px;
+  }
+</style>
