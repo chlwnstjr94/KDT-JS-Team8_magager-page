cf512ac9a51706f2e7a052c968e31b3b83b16d4f
chlwnstjr94 <chlwnstjr940404@gmail.com>
2022-07-11 23:03:53 +0900

    feat: Add deleteProduct

--- src/components/ProductsManagementList.vue
+++ src/components/ProductsManagementList.vue
@@ -1,12 +1,39 @@
 <template>
-  <RouterLink to="/editproduct">
+  <RouterLink :to="`/editproduct/${product.id}`">
     <ion-icon name="create-outline"></ion-icon>
   </RouterLink>
+  <!-- <RouterLink :to="{
+    path: `/editproduct/${product.id}`,
+    name: 'EditProduct',
+    // params: {
+    //   id: product.id,
+    //   title: product.title,
+    //   price: product.price,
+    //   description: product.description,
+    //   tags: product.tags,
+    //   thumbnailBase64: product.thumbnailBase64, 
+    //   photoBase64: product.photoBase64
+    // }
+    params:{ 
+      id: product.id,
+      oldTitle: product.title,
+      oldPrice: product.price,
+      oldDescription: product.description,
+      oldTags: product.tags.toString(),
+      oldThumbnail: product.thumbnail
+    }
+  }">
+    <ion-icon name="create-outline"></ion-icon>
+  </RouterLink> -->
   <img 
     v-if="product.thumbnail"
     :src="product.thumbnail" 
     :alt="product.title">
   <h4>{{ product.title}}</h4>
+
+  <button @click="deleteProduct(product.id)">
+    <ion-icon name="trash-outline"></ion-icon>
+  </button>
 </template>
 
 <script>
@@ -22,6 +49,11 @@ export default {
   },
   computed: {
     ...mapStores(useIndexStore)
+  },
+  methods: {
+    deleteProduct(id) {
+      this.indexStore.deleteProduct(id)
+    }
   }
 }
 </script>
