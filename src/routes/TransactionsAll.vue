56e03451af7858fc0bef68320475f41d44588b1b
chlwnstjr94 <chlwnstjr940404@gmail.com>
2022-07-15 03:35:23 +0900

    feat: Add checkbox in TransactionsAll

--- src/routes/TransactionsAll.vue
+++ src/routes/TransactionsAll.vue
@@ -1,17 +1,28 @@
 <template>
-  <h1>전체 판매 내역</h1>
+  <main>
+    <h1>전체 판매 내역</h1>
 
-  <ul>
-    <li
-      v-for="product in indexStore.allTransactions"
-      :key="product.id"
-      :product="product">
-      {{ product.title }}
-      {{ product.user.email }}
-      {{ product.user.displayName }}
-      {{ product.user.profileImg }}
-    </li>
-  </ul>
+    <ul>
+      <li
+        v-for="product in indexStore.allTransactions"
+        :key="product.id"
+        :product="product">
+        {{ product.title }}
+        {{ product.user.email }}
+        {{ product.user.displayName }}
+        {{ product.user.profileImg }}
+
+        <div class="isCanceled">
+          <input type="checkbox" id="isCanceled" class="checkbox" v-model="canceled">
+          <label for="isCanceled">isCanceled: {{ isCanceled }}</label>
+        </div>
+        <div class="done">
+          <input type="checkbox" id="done" class="checkbox" v-model="done">
+          <label for="done">done: {{ done }}</label>
+        </div>
+      </li>
+    </ul>
+  </main>
 </template>
 
 <script>
@@ -19,22 +30,60 @@ import { mapStores } from 'pinia'
 import { useIndexStore } from '~/store'
 
 export default {
+  data() {
+    return {
+      isCanceled: false,
+      done: false
+    }
+  },
   computed: {
     ...mapStores(useIndexStore)
   },
+  created() {
+    this.indexStore.allTransactions()
+  },
   methods: {
-    allTransactions() {
-      this.indexStore.allTransactions({
-        detailId: this.detailId,
-        user: this.user,
-        account: this.account,
-        product: this.product,
-        reservation: this.reservation,
-        timePaid: this.timePaid,
-        isCanceled: this.isCanceled,
-        done: this.done
-      })
+    canceled() {
+      const detailId = this.$route.params.detailId
+      if (this.isCanceled) {
+        this.indexStore.transactionsCanceled({
+          detailId,
+          isCanceled: true
+        })
+      } else {
+        this.indexStore.transactionsCanceled({
+          detailId,
+          isCanceled: false
+        })
+      }
+    },
+    done() {
+      const detailId = this.$route.params.detailId
+      if (this.done) {
+        this.indexStore.transactionsCanceled({
+          detailId,
+          done: true
+        })
+      } else {
+        this.indexStore.transactionsCanceled({
+          detailId,
+          done: false
+        })
+      }
     }
   }
 }
 </script>
+
+<style lang="scss" scoped>
+main {
+  width: 100%;
+  min-height: 100vh;
+  background-color: #eee;
+}
+.checkbox {
+  width: 10px;
+  height: 10px;
+  background: #000;
+}
+</style>
