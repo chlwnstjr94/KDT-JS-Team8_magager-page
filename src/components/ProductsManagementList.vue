4073c409779a2a96bf12b0d4d72ab65c3a891810
chlwnstjr94 <chlwnstjr940404@gmail.com>
2022-07-19 17:52:41 +0900

    feat: Add style in ProductsManagementList

--- src/components/ProductsManagementList.vue
+++ src/components/ProductsManagementList.vue
@@ -1,34 +1,54 @@
 <template>
-  <button>
-    <RouterLink :to="{
-      name: 'EditProduct',
-      params:{ 
-        id: product.id,
-        oldTitle: product.title,
-        oldPrice: product.price,
-        oldDescription: product.description,
-        oldTags: product.tags.toString(),
-        oldThumbnailBase64: product.thumbnail,
-        oldIsSoldout: product.isSoldout
-      }
-    }">
-      <ion-icon name="create-outline"></ion-icon>
-    </RouterLink>
-  </button>
-  <!-- <button
-  @click="$router.push(`/editproduct/${product.id}`)">
-    <ion-icon name="create-outline"></ion-icon>
-  </button> -->
-  <img 
-    v-if="product.thumbnail"
-    :src="product.thumbnail" 
-    :alt="product.title">
-  <h4>{{ product.title }}</h4>
-  <h4>{{ product.price.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",") + '원' }}</h4>
-
-  <button @click="deleteProduct(product.id)">
-    <ion-icon name="trash-outline"></ion-icon>
-  </button>
+  <tr class="product-list">
+    <td class="edit box">
+      <button class="edit-btn">
+        <RouterLink :to="{
+          name: 'EditProduct',
+          params:{ 
+            id: product.id,
+            oldTitle: product.title,
+            oldPrice: product.price,
+            oldDescription: product.description,
+            oldTags: product.tags.toString(),
+            oldThumbnailBase64: product.thumbnail,
+            oldIsSoldOut: product.isSoldOut
+          }
+        }">
+          <ion-icon name="create-outline"></ion-icon>
+        </RouterLink>
+      </button>
+    </td>
+    <td class="thumbnail box">
+      <img 
+      v-if="product.thumbnail"
+      :src="product.thumbnail" 
+      :alt="product.title">
+    </td>
+    <td class="title box">
+      {{ product.title }}
+    </td>
+    <td class="price box">
+      {{ product.price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',') + ' 원' }}
+    </td>
+    <td class="tags box">
+      <span 
+      v-for="tag in product.tags"
+      :key="tag">
+        # {{ tag }}
+      </span>
+    </td>
+    <td class="description box">
+      {{ product.description }}
+    </td>
+    <td class="soldout box">
+      {{ product.isSoldOut }}
+    </td>
+    <td class="delete box">
+      <button class="delete-btn" @click="deleteProduct(product.id)">
+        <ion-icon name="trash-outline"></ion-icon>
+      </button>
+    </td>
+  </tr>
 </template>
 
 <script>
@@ -45,6 +65,9 @@ export default {
   computed: {
     ...mapStores(useIndexStore)
   },
+  create() {
+    console.log(this.product.photo)
+  },
   methods: {
     deleteProduct(id) {
       this.indexStore.deleteProduct(id)
@@ -54,8 +77,53 @@ export default {
 </script>
 
 <style scoped lang="scss">
+.product-list {
+  width: 100%;
+  td {
+    border: 1px solid #e4e4e4;
+    height: 100px;
+    padding: 10px;
+  }
+  .box {
+    vertical-align: middle;
+    text-align: center;
+    &.tags {
+      span {
+        display: inline-block;
+        margin-left: 4px;
+        &:first-child {
+          margin-left: 0;
+        }
+      }
+    }
+    &.description {
+      text-overflow: ellipsis;
+      overflow: hidden;
+    }
+    .edit-btn, .delete-btn {
+      width: 50px;
+      height: 50px;
+      border: 1px solid #2d2e32;
+      border-radius: 10px;
+      transition: 0.3s;
+      ion-icon {
+        width: 20px;
+        height: 20px;
+        color: #2d2e32;
+      }
+      &:hover {
+        background: #ff9900;
+        border: 1px solid #f97400;
+        transition: 0.3s;
+        ion-icon {
+          color: #fcfcfc;
+        }
+      }
+    }
+  }
+}
 img {
-  width: 100px;
+  min-width: 100px;
   height: 100px;
 }
 </style>
