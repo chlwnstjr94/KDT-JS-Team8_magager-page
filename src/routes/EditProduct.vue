bc91b0c604ad60dbf132a7a019a53741879628a8
chlwnstjr94 <chlwnstjr940404@gmail.com>
2022-07-20 03:51:22 +0900

    feat: Modify template & Add style in EditProduct

--- src/routes/EditProduct.vue
+++ src/routes/EditProduct.vue
@@ -1,42 +1,87 @@
 <template>
   <main>
     <section>
-      <h1>제품 수정</h1>
+      <div class="head-title">
+        <h2 class="page-title">제품 수정</h2>
+      </div>
 
-      <form @submit.prevent="editProduct()">
-        <article class="info-box">
-          <p>이모티콘 썸네일</p>
-          <img :src="thumbnailBase64" :alt="title">
-          <input type="file" @change="thumbnailBase64Img">
+      <form @submit.prevent="editProduct()" class="contents">
+        <h3 class="product-info">이모티콘 정보</h3>
+
+        <article class="item-box thumbnail-box">
+          <div class="thumbnail-input">
+            <label>
+              <span class="title">이모티콘 썸네일</span>
+            </label>
+            <input class="input-file thumbnail" type="file" @change="thumbnailBase64Img">
+          </div>
+          <div v-if="thumbnailBase64" class="img-box">
+            <img :src="thumbnailBase64" :alt="title">
+          </div>
+          <div v-else class="img-box">
+            <ion-icon name="cloud-upload-outline"></ion-icon>
+          </div>
         </article>
-        <article class="info-box">
-          <p>이모티콘 상품명</p>
-          <input type="text" v-model="title">
+
+        <article class="item-box">
+          <label>
+            <span class="title">이모티콘 상품명</span>
+          </label>
+          <input placeholder="상품명을 입력해주세요." class="input-text" type="text" v-model="title">
         </article>
-        <article class="info-box">
-          <p>이모티콘 태그명</p>
-          <input type="text" v-model="tags">
+
+        <article class="item-box">
+          <label>
+            <span class="title">이모티콘 태그</span>
+          </label>
+          <input placeholder="태그를 입력해주세요." class="input-text" type="text" v-model="tags">
         </article>
-        <article class="info-box">
-          <p>이모티콘 상세설명</p>
-          <input type="text" v-model="description">
+
+        <article class="item-box description-box">
+          <label>
+            <span class="title">이모티콘 상세설명</span>
+          </label>
+          <textarea placeholder="상세 설명을 입력해주세요." class="input-text" type="text" v-model="description"></textarea>
         </article>
-        <article class="info-box">
-          <p>이모티콘 가격</p>
-          <input type="text" v-model.number="price">
+
+        <article class="item-box">
+          <label>
+            <span class="title">이모티콘 가격</span>
+          </label>
+          <input placeholder="가격을 입력해주세요." class="input-text" type="text" v-model.number="price">
         </article>
-        <article class="info-box">
-          <p v-if="!isSoldOut">이모티콘 판매중</p>
-          <p v-else>이모티콘 매진</p>
-          <input type="checkbox" class="checkbox" v-model="isSoldOut">
+
+        <article class="item-box">
+          <label for="isSoldOut">
+            <span v-if="!isSoldOut" class="title">이모티콘 판매중</span>
+            <span v-else class="title">이모티콘 매진</span>
+          </label>
+          <label class="checkbox-box">
+            <input type="checkbox" id="isSoldOut" class="checkbox" v-model="isSoldOut">
+            <span>
+              <i></i>
+            </span>
+          </label>
         </article>
-        <article class="info-box">
-          <p>이모티콘 상세 사진</p>
-          <img v-if="!photoBase64" :src="indexStore.product.photo" :alt="title">
-          <img v-else :src="photoBase64" :alt="title">
-          <input type="file" @change="photoBase64Img">
+        
+        <article class="item-box">
+          <label>
+            <span class="title">이모티콘 상세 사진</span>
+          </label>
+          <input class="input-file" type="file" @change="photoBase64Img">
         </article>
-        <button>수정 완료</button>
+        <div class="photo-box">
+          <div v-if="!photoBase64" class="img-box">
+            <img :src="indexStore.product.photo" :alt="title">
+          </div>
+          <div v-else class="img-box">
+            <img :src="photoBase64" :alt="title">
+          </div>
+        </div>
+
+        <div class="btn-wrap">
+          <button class="edit-btn">수정 완료</button>
+        </div>
       </form>
     </section>
   </main>
@@ -137,9 +182,206 @@ export default {
 main {
   width: 100%;
   min-height: 100vh;
-  background-color: #eee;
+  background-color: #fff;
+  border: 1px solid #e6e6e6;
+  padding: 0 40px 60px;
+  box-sizing: border-box;
+  .head-title {
+    padding: 24px 0 8px;
+    margin-bottom: 38px;
+    border-bottom: 1px solid #2d2e32;
+    position: relative;
+    .page-title {
+      padding: 18px 0 0;
+      font-size: 32px;
+    }
+  }
+  .contents {
+    .product-info {
+      margin-bottom: 12px;
+      padding-top: 38px;
+      font-size: 24px;
+    }
+    .item-box {
+      position: relative;
+      padding: 5px 0 5px 160px;
+      display: flex;
+      align-items: center;
+      &.thumbnail-box {
+        align-items: flex-start;
+        .thumbnail-input {
+          display: flex;
+          align-items: center;
+        }
+      }
+      &.description-box {
+        align-items: flex-start;
+        .input-text {
+          width: 100%;
+          height: 80px;
+        }
+      }
+      label {
+        position: absolute;
+        left: 0;
+        display: flex;
+        align-items: center;
+        .title {
+          font-size: 13px;
+          font-weight: bold;
+        }
+      }
+      .img-box {
+        width: 180px;
+        height: 180px;
+        padding: 10px;
+        border: 4px dashed #e4e4e4;
+        border-radius: 20px;
+        margin-left: 20px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        img {
+          object-fit: cover;
+        }
+        ion-icon {
+          width: 100%;
+          height: 100%;
+          color: #e4e4e4;
+        }
+      }
+      .input-text {
+        width: 380px;
+        height: 40px;
+        padding: 10px 15px;
+        border: 1px solid #e4e4e4;
+        border-radius: 10px;
+        background-color: #fcfcfc;
+        color: #b2b2b2;
+        box-sizing: border-box;
+        transition: 0.5s;
+        &::placeholder {
+          font-size: 13px;
+          color: #b2b2b2;
+        }
+        &:focus {
+          background-color: #fff;
+          color: #2d2e32;
+          border: 1px solid #2d2e32;
+        }
+      }
+      .input-file {
+        width: 380px;
+        height: 40px;
+        padding: 10px 15px;
+        border: 1px solid #e4e4e4;
+        border-radius: 10px;
+        background-color: #fcfcfc;
+        color: #b2b2b2;
+        box-sizing: border-box;
+        &.thumbnail {
+          width: 300px;
+        }
+        &::file-selector-button {
+          display: none;
+        }
+      }
+      .checkbox-box {
+        display: inline-block;
+        position: relative;
+        width: 60px;
+        height: 30px;
+        cursor: pointer;
+        .checkbox {
+          position: relative;
+          z-index: 1;
+          appearance: none;
+          display: none;
+          &:checked {
+            & ~ span {
+              background: #05be05;
+              i {
+                left: 33px;
+              }
+            }
+          }
+        }
+        span {
+          position: absolute;
+          top: 0;
+          left: 0;
+          width: 100%;
+          height: 100%;
+          background: #fe0000;
+          border-radius: 30px;
+          transition: 0.5s;
+          i {
+            position: absolute;
+            top: 4px;
+            left: 5px;
+            width: 22px;
+            height: 22px;
+            background: #fff;
+            border-radius: 50%;
+            transition: 0.5s;
+          }
+        }
+      }
+    }
+    .photo-box {
+      margin-top: 20px;
+      border-top: 1px solid #e4e4e4;
+      .img-box {
+        width: 100%;
+        margin-top: 30px;
+        padding: 10px;
+        border: 4px dashed #e4e4e4;
+        border-radius: 20px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        position: relative;
+        &:after {
+          content: "";
+          display: block;
+          padding-bottom: 100%;
+        }
+        img {
+          position: absolute;
+          width: 90%;
+          height: 90%;
+          object-fit: cover;
+        }
+        ion-icon {
+          position: absolute;
+          width: 90%;
+          height: 90%;
+          color: #e4e4e4;
+        }
+      }
+    }
+  }
+  .btn-wrap {
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    margin-top: 40px;
+    .edit-btn {
+      width: 256px;
+      height: 64px;
+      font-size: 18px;
+      color: #1e1e1e;
+      background: #fcd207;
+      border: 1px solid #ffbb1b;
+      border-radius: 10px;
+      transition: 0.5s;
+      &:hover {
+        background: #efc706;
+        border-color: #f2b119;
+      }
+    }
+  }
 }
-
 .checkbox {
   width: 10px;
   height: 10px;
