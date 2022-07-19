a2db9c515c20d30cb10cf8e65d693b61b89c7eae
chlwnstjr94 <chlwnstjr940404@gmail.com>
2022-07-19 23:57:04 +0900

    feat: Add style in AllProduct.vue

--- src/routes/AllProduct.vue
+++ src/routes/AllProduct.vue
@@ -1,13 +1,31 @@
 <template>
   <main>
-    <h1>제품 관리</h1>
+    <h2 class="page-title">제품 관리</h2>
 
-    <ul>
-      <ProductsManagementList
+    <table>
+      <thead>
+        <tr>
+          <th class="table-title"><span>제품 수정</span></th>
+          <th class="table-title"><span>제품 썸네일</span></th>
+          <th class="table-title"><span>제품명</span></th>
+          <th class="table-title"><span>제품 가격</span></th>
+          <th class="table-title"><span>제품 태그</span></th>
+          <th class="table-title"><span>제품 상세 내용</span></th>
+          <th class="table-title"><span>제품 판매/매진</span></th>
+          <th class="table-title"><span>제품 삭제</span></th>
+        </tr>
+      </thead>
+      <tbody>
+        <ProductsManagementList
         v-for="product in indexStore.allProducts"
         :key="product.id"
         :product="product"/>
-    </ul>
+      </tbody>
+    </table>
+
+    <!-- <ul>
+      
+    </ul> -->
   </main>
 </template>
 
@@ -46,6 +64,31 @@ export default {
 main {
   width: 100%;
   min-height: 100vh;
-  background-color: #eee;
+  background-color: #fff;
+  border: 1px solid #e6e6e6;
+  padding: 0 40px 60px;
+  box-sizing: border-box;
+  .page-title {
+    padding: 42px 0 8px;
+    margin-bottom: 38px;
+    border-bottom: 1px solid #2d2e32;
+    font-size: 32px;
+    position: relative;
+  }
+  table {
+    width: 100%;
+    border: 1px solid #e4e4e4;
+    tr, th {
+      border: 1px solid #e4e4e4;
+    }
+    th {
+      padding: 10px;
+    }
+    .table-title {
+      min-width: 100px;
+      text-align: center;
+      vertical-align: middle;
+    }
+  }
 }
 </style>
