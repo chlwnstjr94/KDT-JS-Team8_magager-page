de1de86d0edc9c2c31f82c4c2e093cc0702ad816
chlwnstjr94 <chlwnstjr940404@gmail.com>
2022-07-21 19:39:16 +0900

    feat: Modify guards.js

--- src/routes/guards.js
+++ src/routes/guards.js
@@ -3,6 +3,23 @@ import { validateTokenUser } from '~/core'
 import { useUserStore } from '~/store/user'
 
 router.beforeEach(async (to) => {
+  if (to.path.includes('/addproduct')) {
+    // 토큰 없는 경우
+    const accessToken = window.localStorage.getItem('token')
+    if (!accessToken) {
+      console.log('로그인 해주세요!')
+      return '/login'
+    } else {
+      const user = await validateTokenUser(accessToken)
+      if (user.data.email === 'testAdmin@admin.com') {
+        return true
+      } else {
+        console.log('관리자가 아닙니다!')
+        useUserStore().logoutUser()
+        return '/login'
+      }
+    }
+  }
   if (to.path.includes('/allproduct')) {
     // 토큰 없는 경우
     const accessToken = window.localStorage.getItem('token')
